769f36fc9d3d93352d83c44d794e1793dff2cdcd
prebird <78974381+prebird@users.noreply.github.com>
2025-01-29 22:58:40 +0900

    todo 리스트 만들기
    - JSX
    - JSX 반복문, 조건문(3항연산자)
    - 이벤트로 State 변경하기
    - state 변경시 불변성 지키기 (배열의 경우 ...)
    - state 변경 감지를 위해 useEffect 훅 사용하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useEffect, useState} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState([
+    {id:1, title: '아침 챙겨먹기', completed: true},
+    {id:2, title: '점심 챙겨먹기', completed: false},
+    {id:3, title: '저녁 챙겨먹기', completed: false},
+  ]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const onChangeNewTodo = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTodo(e.target.value);
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault(); // SPA는 페이지가 새로고침 되는 것을 왠만해서는 막아야한다.
+    const nextTodo = [...todo, {id: todo.length + 1, title: newTodo, completed: false}]
+    setTodo(nextTodo);
+    /* 불변성, 참조가 다를 때만 react 가 값의 변경을 인식한다. */
+    /* immer 라이브러리를 사용하면 코드가 깔끔해진다. */
+
+    // console.log('changedTODO', todo); -> 안바뀜, 비동기때문 X (batch)
+  }
+
+  useEffect(() => {
+    // todo 가 바뀌었는지 감지하는 함수
+    console.log('changedTODO', todo);
+  }, [todo]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <>  {/* Fragment - 빈 괄호*/}
+      <div>TODO</div>
+      {todo.length === 0 ? ( // JSX 에서는 if 문을 쓸 수 없으므로 3항연산자를 쓴다.
+          <>
+            <div>할일을 추가해 보세요.</div>
+            <form onSubmit={onSubmit}>
+              <input value={newTodo} onChange={onChangeNewTodo}/>
+              <button>추가</button>
+            </form>
+          </>
+      ) : (
+          <>
+            {todo.map((t) => (<div key={t.id}>{t.title}</div>))}
+            <form onSubmit={onSubmit}>
+              <input value={newTodo} onChange={onChangeNewTodo}/>
+              <button>추가</button>
+            </form>
+          </>
+      )}
     </>
   )
 }
